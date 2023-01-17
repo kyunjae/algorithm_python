ec3e99732f8b7dea1a6e084ff588b98367c19ad0
강윤재 <dbsworkd23@naver.com>
2023-01-17 14:13:36 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+result = 0
+
+for _ in range(n):
+    result += min(A) * max(B)
+    A.remove(min(A))
+    B.remove(max(B))
+
+print(result)
